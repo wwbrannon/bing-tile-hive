7b64f2160d4f09f935719dce0db3e59b6d5f2a3f
William Brannon <will.brannon@gmail.com>
2018-06-25 15:02:25 -0400

    more TilesAround tests

--- src/test/java/com/wwbrannon/bing/TestBingTilesAround.java
+++ src/test/java/com/wwbrannon/bing/TestBingTilesAround.java
@@ -1,48 +1,38 @@
 package com.wwbrannon.bing;
 
-import org.testng.annotations.Test;
-import org.testng.Assert;
+import java.util.Arrays;
+import java.util.ArrayList;
 
-import com.google.common.collect.ImmutableList;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.testng.annotations.Test;
 
 import com.wwbrannon.bing.*;
 import com.wwbrannon.bing.exception.BingTileException;
 
+import static org.testng.Assert.*;
+
 public class TestBingTilesAround
 {
     @Test
-    public void test()
+    public void test() throws BingTileException
     {
-        assertFunction(
-                "transform(bing_tiles_around(30.12, 60, 1), x -> bing_tile_quadkey(x))",
-                new ArrayType(VARCHAR),
-                ImmutableList.of("0", "2", "1", "3"));
-        assertFunction(
-                "transform(bing_tiles_around(30.12, 60, 15), x -> bing_tile_quadkey(x))",
-                new ArrayType(VARCHAR),
-                ImmutableList.of(
-                        "123030123010102",
-                        "123030123010120",
-                        "123030123010122",
-                        "123030123010103",
-                        "123030123010121",
-                        "123030123010123",
-                        "123030123010112",
-                        "123030123010130",
-                        "123030123010132"));
-        assertFunction(
-                "transform(bing_tiles_around(30.12, 60, 23), x -> bing_tile_quadkey(x))",
-                new ArrayType(VARCHAR),
-                ImmutableList.of(
-                        "12303012301012121210122",
-                        "12303012301012121210300",
-                        "12303012301012121210302",
-                        "12303012301012121210123",
-                        "12303012301012121210301",
-                        "12303012301012121210303",
-                        "12303012301012121210132",
-                        "12303012301012121210310",
-                        "12303012301012121210312"));
+        assertEquals((new BT_TilesAround()).evaluate(new DoubleWritable(30.12), new DoubleWritable(60), new IntWritable(1)),
+                     new ArrayList<Text>(Arrays.asList(new Text("0"), new Text("2"), new Text("1"), new Text("3"))));
+        
+        assertEquals((new BT_TilesAround()).evaluate(new DoubleWritable(30.12), new DoubleWritable(60), new IntWritable(15)),
+                     new ArrayList<Text>(Arrays.asList(new Text("123030123010102"), new Text("123030123010120"), new Text("123030123010122"),
+                                                       new Text("123030123010103"), new Text("123030123010121"), new Text("123030123010123"),
+                                                       new Text("123030123010112"), new Text("123030123010130"), new Text("123030123010132"))));
+        
+        assertEquals((new BT_TilesAround()).evaluate(new DoubleWritable(30.12), new DoubleWritable(60), new IntWritable(23)),
+                     new ArrayList<Text>(Arrays.asList(new Text("12303012301012121210122"), new Text("12303012301012121210300"),
+                                                       new Text("12303012301012121210302"), new Text("12303012301012121210123"),
+                                                       new Text("12303012301012121210301"), new Text("12303012301012121210303"),
+                                                       new Text("12303012301012121210132"), new Text("12303012301012121210310"),
+                                                       new Text("12303012301012121210312"))));
     }
 }
 
