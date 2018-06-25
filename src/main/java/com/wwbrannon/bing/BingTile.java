5af3a54cfb119e8377171e90b68888f4c40757dc
William Brannon <will.brannon@gmail.com>
2018-06-25 02:55:00 -0400

    restore json annotations

--- src/main/java/com/wwbrannon/bing/BingTile.java
+++ src/main/java/com/wwbrannon/bing/BingTile.java
@@ -7,6 +7,9 @@ import java.util.List;
 import java.lang.Math;
 import java.lang.String;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import com.esri.core.geometry.Envelope;
 import com.esri.core.geometry.Point;
 import com.esri.core.geometry.ogc.OGCGeometry;
@@ -346,7 +349,12 @@ public final class BingTile
         return new BingTile(tileX, tileY, zoomLevel);
     }
     
-    public static BingTile fromCoordinates(int x, int y, int zoomLevel) throws BingTileException
+    @JsonCreator
+    public static BingTile fromCoordinates(
+        @JsonProperty("x") int x,
+        @JsonProperty("y") int y,
+        @JsonProperty("zoom") int zoomLevel
+    ) throws BingTileException
     {
         return new BingTile(x, y, zoomLevel);
     }
@@ -488,16 +496,19 @@ public final class BingTile
      * Accessor methods
      */
 
+    @JsonProperty
     public int getX()
     {
         return x;
     }
 
+    @JsonProperty
     public int getY()
     {
         return y;
     }
 
+    @JsonProperty("zoom")
     public int getZoomLevel()
     {
         return zoomLevel;
